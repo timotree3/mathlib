0e9ba169e7036a2441fded28e97f0f76a0ea6d33
Violeta Hernández <vi.hdz.p@gmail.com>
2022-03-13 03:03:33 -0600

    tweaks

--- src/analysis/convex/radon.lean
+++ src/analysis/convex/radon.lean
@@ -18,21 +18,23 @@ begin
   -- We choose `M₁` and `M₂` as the sets of points in this combination with positive and negative
   -- coefficients respectively.
   let I₁ := M.filter (λ i : ι, 0 < f i),
-  let I₂ := M.filter (λ i : ι, ¬ 0 < f i),
+  let I₂ := M.filter (λ i : ι, f i ≤ 0),
   refine ⟨p '' I₁, set.image_subset_range p I₁, p '' I₂, set.image_subset_range p I₂, _, _⟩,
   { rw set.disjoint_iff_forall_ne,
     rintros _ ⟨i, hi, rfl⟩ _ ⟨j, hj, rfl⟩ h,
     rw hp h at hi,
-    exact (finset.mem_filter.1 hj).2 (finset.mem_filter.1 hi).2 },
+    exact (finset.mem_filter.1 hj).2.not_lt (finset.mem_filter.1 hi).2 },
 
   -- `∑ x in I₁, (f x / k) • p x = ∑ x in I₂, (- f x / k) • p x` is in both convex hulls.
   { rw set.not_disjoint_iff,
     let k := ∑ x in I₁, f x,
     have HI₁ : ∀ j, j ∈ I₁ → 0 < f j := λ j hj, (finset.mem_filter.1 hj).2,
     have HI₁' : ∀ j, j ∈ I₁ → 0 ≤ f j := λ j hj, (HI₁ j hj).le,
-    have HI₂ : ∀ j, j ∈ I₂ → f j ≤ 0 := λ j hj, le_of_not_lt $ (finset.mem_filter.1 hj).2,
+    have HI₂ : ∀ j, j ∈ I₂ → f j ≤ 0 := λ j hj, (finset.mem_filter.1 hj).2,
     have hk : 0 ≤ k := finset.sum_nonneg HI₁',
     have Hnn : ∀ j, j ∈ I₁ → 0 ≤ f j / k := λ i hi, div_nonneg (HI₁' i hi) hk,
+
+    -- Surely this can be golfed somewhat??
     have HI₁₂ : I₁ = M \ I₂,
     { ext i,
       refine ⟨λ hi, _, λ hi, _⟩,
@@ -40,13 +42,17 @@ begin
         exact ⟨(finset.mem_filter.1 hi).1, λ hi', (HI₁ i hi).not_le (HI₂ i hi')⟩ },
       { rw finset.mem_sdiff at hi,
         refine finset.mem_filter.2 ⟨hi.1, _⟩,
-        by_contra hi',
+        by_contra' hi',
         exact hi.2 (finset.mem_filter.2 ⟨hi.1, hi'⟩) } },
+        
     have HS : ∑ i in I₁, f i / k = 1,
     { rw ←finset.sum_div,
       refine div_self (ne_of_gt (finset.sum_pos HI₁ _)),
       { by_contra H,
         rw finset.not_nonempty_iff_eq_empty at H,
+
+        -- Since every term in `∑ i in M, f i` sum is nonnegative, and `f a ≠ 0`, then the sum is
+        -- negative, contradicting `hf`.
         sorry } },
     refine ⟨∑ x in I₁, (f x / k) • p x, _, _⟩,
     { rw finset.coe_filter,
@@ -56,9 +62,11 @@ begin
       { simp_rw [neg_div, neg_smul],
         rw [finset.sum_neg_distrib, eq_neg_iff_add_eq_zero, HI₁₂,
           finset.sum_sdiff (finset.filter_subset _ _)],
-        simp_rw [div_eq_mul_inv, mul_comm _ (k⁻¹), mul_smul],
+        simp_rw [div_eq_mul_inv, mul_comm _ k⁻¹, mul_smul],
         rw ←finset.smul_sum,
         convert smul_zero _,
+
+        -- This should be an immediate consequence of the affine dependence.
         sorry },
       rw HS₁₂,
       refine convex.sum_mem (convex_convex_hull _ _) (λ i hi, div_nonneg (le_neg_of_le_neg _) hk) _
