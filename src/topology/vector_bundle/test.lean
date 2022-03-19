6f2abad0dc5c40d997a85ad021cb21888bb3300b
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-03-18 20:01:11 -0400

    build up toy

--- src/topology/vector_bundle/test.lean
+++ src/topology/vector_bundle/test.lean
@@ -1,57 +1,92 @@
 import topology.vector_bundle.basic
-import analysis.normed_space.operator_norm
 import analysis.normed_space.bounded_linear_maps
 
-open bundle
+open bundle set
 
 variables {𝕜 : Type*} [nondiscrete_normed_field 𝕜]
-
 variables {B : Type*} [topological_space B]
 
+/-! ## Operator norm -/
+
+section
 variables {F₁ : Type*} [normed_group F₁] [normed_space 𝕜 F₁]
-  {E₁ : B → Type*} [Π x, add_comm_monoid (E₁ x)] [Π x, module 𝕜 (E₁ x)]
-  [Π x : B, topological_space (E₁ x)] [topological_space (total_space E₁)]
-  [Π x, has_continuous_add (E₁ x)] [Π x, has_continuous_smul 𝕜 (E₁ x)]
-  [topological_vector_bundle 𝕜 F₁ E₁]
+variables {F₂ : Type*} [normed_group F₂] [normed_space 𝕜 F₂]
+variables {F₃ : Type*} [normed_group F₃] [normed_space 𝕜 F₃]
 
-variables {F₂ : Type*} [normed_group F₂][normed_space 𝕜 F₂]
-  {E₂ : B → Type*} [Π x, add_comm_monoid (E₂ x)] [Π x, module 𝕜 (E₂ x)]
-  [Π x : B, topological_space (E₂ x)] [topological_space (total_space E₂)]
-  [Π x, has_continuous_add (E₂ x)] [Π x, has_continuous_smul 𝕜 (E₂ x)]
-  [topological_vector_bundle 𝕜 F₂ E₂]
-
--- might actually be false for the norm topology?
-lemma continuous_transition (e : homeomorph (B × F₁) (B × F₁))
-  (ε : B → F₁ ≃L[𝕜] F₁) (h : ∀ x f, (x, ε x f) = e (x, f)) :
-  continuous (λ x, (ε x : F₁ →L[𝕜] F₁)) :=
-sorry
-
-example (E F G : Type*) [normed_group E] [normed_group F] [normed_group G] [normed_space 𝕜 E]
-  [normed_space 𝕜 F] [normed_space 𝕜 G] :
-  continuous (λ p : (E →L[𝕜] F) × (F →L[𝕜] G), p.2.comp p.1) :=
-is_bounded_bilinear_map_comp.continuous
-
-example (E F G : Type*) [normed_group E] [normed_group F] [normed_group G] [normed_space 𝕜 E]
-  [normed_space 𝕜 F] [normed_space 𝕜 G] :
-  continuous (λ p : (F →L[𝕜] F) × (F →L[𝕜] G), p.2.comp p.1) :=
-by library_search
-
-example (e₁ : homeomorph (B × F₁) (B × F₁)) (e₂ : homeomorph (B × F₂) (B × F₂))
-  (ε₁ : B → F₁ ≃L[𝕜] F₁) (ε₂ : B → F₂ ≃L[𝕜] F₂) (h₁ : ∀ x f, (x, ε₁ x f) = e₁ (x, f))
-  (h₂ : ∀ x f, (x, ε₂ x f) = e₂ (x, f)) :
-  continuous (λ p : B × (F₁ →L[𝕜] F₂),
-    continuous_linear_map.compL 𝕜 F₁ F₁ F₂
-      (continuous_linear_map.compL 𝕜 F₁ F₂ F₂
-        (ε₂ p.1 : F₂ →L[𝕜] F₂)
-        p.2)
-      (ε₁ p.1 : F₁ →L[𝕜] F₁)) :=
+lemma pre (ε : B → (F₁ →L[𝕜] F₂)) (h : continuous (λ p : B × F₁, ε p.1 p.2)) :
+  continuous (λ p : B × (F₂ →L[𝕜] F₃), p.2 ∘L ε p.1) :=
 begin
-  let Φ : (F₁ →L[𝕜] F₁) × (F₁ →L[𝕜] F₂) × (F₂ →L[𝕜] F₂) → F₁ →L[𝕜] F₂ := λ p, p.2.2 ∘L p.2.1 ∘L p.1,
-  have H : continuous Φ := sorry,
-  have H' : continuous
-    (λ p : B × (F₁ →L[𝕜] F₂), ((ε₁ p.1 : F₁ →L[𝕜] F₁), p.2, (ε₂ p.1 : F₂ →L[𝕜] F₂))),
-  { refine ((continuous_transition e₁ ε₁ h₁).comp continuous_fst).prod_mk _,
-    refine continuous_snd.prod_mk _,
-    exact (continuous_transition e₂ ε₂ h₂).comp continuous_fst },
-  exact H.comp H',
+  have hε : continuous ε := sorry,
+  exact is_bounded_bilinear_map_comp.continuous.comp (hε.prod_map continuous_id),
+end
+
+lemma post (ε : B → (F₂ →L[𝕜] F₃)) (h : continuous (λ p : B × F₂, ε p.1 p.2)) :
+  continuous (λ p : B × (F₁ →L[𝕜] F₂), ε p.1 ∘L p.2) :=
+begin
+  have hε : continuous ε := sorry, -- would work in topology of pointwise convergence, see `***` below
+  exact is_bounded_bilinear_map_comp.continuous.comp (continuous_snd.prod_mk (hε.comp continuous_fst)),
+end
+
+example (ε₁ : B → (F₁ ≃L[𝕜] F₁)) (h₁ : continuous (λ p : B × F₁, ε₁ p.1 p.2))
+  (ε₂ : B → (F₂ ≃L[𝕜] F₂)) (h₂ : continuous (λ p : B × F₂, ε₂ p.1 p.2)) :
+  continuous (λ p : B × (F₁ →L[𝕜] F₂), (ε₂ p.1 : F₂ →L[𝕜] F₂) ∘L p.2 ∘L (ε₁ p.1 : F₁ →L[𝕜] F₁)) :=
+(post (λ x, (ε₂ x : F₂ →L[𝕜] F₂)) h₂).comp
+  (continuous_fst.prod_mk (pre (λ x, (ε₁ x : F₁ →L[𝕜] F₁)) h₁))
+
+end
+
+/-! ## Topology of pointwise convergence -/
+
+section
+variables {F₁ : Type*} [add_comm_group F₁] [module 𝕜 F₁] [topological_space F₁]
+variables {F₂ : Type*} [add_comm_group F₂] [module 𝕜 F₂] [topological_space F₂]
+variables {F₃ : Type*} [add_comm_group F₃] [module 𝕜 F₃] [topological_space F₃]
+
+/-- The topology of pointwise convergence on `F₁ →L[𝕜] F₂`. -/
+instance : topological_space (F₁ →L[𝕜] F₂) :=
+@topological_space.induced (F₁ →L[𝕜] F₂) (F₁ → F₂) (λ f, (f : F₁ → F₂)) (by apply_instance)
+
+/-- see `***` above -/
+example (ε : B → (F₂ →L[𝕜] F₃)) (h : continuous (λ p : B × F₂, ε p.1 p.2)) : continuous ε :=
+begin
+  apply continuous_induced_rng,
+  rw continuous_iff_continuous_at,
+  intros x,
+  rw continuous_at_pi,
+  intros v,
+  exact (h.comp (continuous_id.prod_mk continuous_const)).continuous_at
+end
+
+lemma pre' (ε : B → (F₁ →L[𝕜] F₂)) (h : continuous (λ p : B × F₁, ε p.1 p.2)) :
+  continuous (λ p : B × (F₂ →L[𝕜] F₃), p.2 ∘L ε p.1) :=
+begin
+  apply continuous_induced_rng,
+  rw continuous_iff_continuous_at,
+  intros x,
+  rw continuous_at_pi,
+  intros v,
+  let Φ : (F₂ →L[𝕜] F₃) × F₂ → F₃ := λ p, p.1 p.2,
+  have hΦ : continuous Φ := sorry, -- would work in operator-norm topology (`is_bounded_bilinear_map_apply`)
+  exact (hΦ.comp (continuous_snd.prod_mk ((h.comp (continuous_id.prod_mk continuous_const)).comp
+    continuous_fst))).continuous_at,
+end
+
+lemma post' (ε : B → (F₂ →L[𝕜] F₃)) (h : continuous (λ p : B × F₂, ε p.1 p.2)) :
+  continuous (λ p : B × (F₁ →L[𝕜] F₂), ε p.1 ∘L p.2) :=
+begin
+  apply continuous_induced_rng,
+  rw continuous_iff_continuous_at,
+  intros x,
+  rw continuous_at_pi,
+  intros v,
+  exact (h.comp (continuous_id.prod_map
+    ((continuous_apply v).comp continuous_induced_dom))).continuous_at,
+end
+
+example (ε₁ : B → (F₁ ≃L[𝕜] F₁)) (h₁ : continuous (λ p : B × F₁, ε₁ p.1 p.2))
+  (ε₂ : B → (F₂ ≃L[𝕜] F₂)) (h₂ : continuous (λ p : B × F₂, ε₂ p.1 p.2)) :
+  continuous (λ p : B × (F₁ →L[𝕜] F₂), (ε₂ p.1 : F₂ →L[𝕜] F₂) ∘L p.2 ∘L (ε₁ p.1 : F₁ →L[𝕜] F₁)) :=
+(post' (λ x, (ε₂ x : F₂ →L[𝕜] F₂)) h₂).comp
+  (continuous_fst.prod_mk (pre' (λ x, (ε₁ x : F₁ →L[𝕜] F₁)) h₁))
+
 end
