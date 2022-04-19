1be0ffd3f6eb113d437f939f44da96dbcd527e79
Floris van Doorn <fpvdoorn@gmail.com>
2022-04-19 20:17:16 +0200

    a lot of new material

--- src/measure_theory/function/strongly_measurable.lean
+++ src/measure_theory/function/strongly_measurable.lean
@@ -1299,6 +1299,11 @@ lemma comp_measurable {γ : Type*} {mγ : measurable_space γ} {mα : measurable
   ae_strongly_measurable (g ∘ f) μ :=
 hg.comp_ae_measurable hf.ae_measurable
 
+lemma comp_measurable' {γ : Type*} {mγ : measurable_space γ} {mα : measurable_space α} {f : γ → α}
+  {μ : measure γ} {ν : measure α} (hg : ae_strongly_measurable g ν) (hf : measurable f)
+  (h : μ.map f ≪ ν) : ae_strongly_measurable (g ∘ f) μ :=
+(hg.mono' h).comp_measurable hf
+
 lemma is_separable_ae_range (hf : ae_strongly_measurable f μ) :
   ∃ (t : set β), is_separable t ∧ ∀ᵐ x ∂μ, f x ∈ t :=
 begin
